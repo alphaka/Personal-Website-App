75687b8f5b5bf3f9bbb00e3e07b9c49a947378ad
Alpha Kaba <63592880+alphaka@users.noreply.github.com>
2021-08-22 00:09:56 -0400

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,6 +1,6 @@
 const mailgun = require("mailgun-js");
-const DOMAIN = "sandboxc85804afeb334bcab03580d6ece85baf.mailgun.org";
-const APIKEY = "4466fb9562970e514d933441d6d36c1f-9776af14-373f9c8f";
+const DOMAIN = "sandboxc....mailgun.org";
+const APIKEY = "4466....";
 const mg = mailgun({ apiKey: APIKEY, domain: DOMAIN });
 
 const sendMail = (name, email, subject, text, cb) => {
